a758137b4c32dc627700eafa5302e82adfb442f9
kparra7491 <kparra7491@gmail.com>
2020-12-17 01:36:54 +0000

    Initial commit

--- src/js/component/home.js
+++ src/js/component/home.js
@@ -1,24 +1,97 @@
 import React from "react";
 
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
-
 //create your first component
-export function Home() {
-	return (
-		<div className="text-center mt-5">
-			<h1>Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
-		</div>
-	);
+export class Home extends React.Component {
+	constructor(props) {
+		super(props);
+		this.player = null;
+		this.state = {
+			songs: [
+				{
+					title: "South Park",
+					id: "south-park",
+					author: "Kyle",
+					url:
+						"https://assets.breatheco.de/apis/sound/files/cartoons/songs/south-park.mp3"
+				},
+				{
+					title: "Thunder Cats",
+					id: "thundercats",
+					author: "Moonra",
+					url:
+						"https://assets.breatheco.de/apis/sound/files/cartoons/songs/thundercats.mp3"
+				},
+				{
+					title: "X-Men",
+					id: "x-men",
+					author: "Profesor",
+					url:
+						"https://assets.breatheco.de/apis/sound/files/cartoons/songs/x-men.mp3"
+				}
+			],
+			currentSong: {},
+			currentAction: <i className="fas fa-play" />,
+			currentButton: "",
+
+			j: 0
+		};
+	}
+
+	playSong = index => {
+		if (this.state.currentbutton !== "play") {
+			this.setState({
+				currentSong: this.state.songs[index],
+				currentAction: <i className="fas fa-play" />,
+				currentButton: "play"
+			});
+			this.player.play();
+		} else {
+			this.setState({
+				currentAction: <i className="fas fa-pause-circle" />,
+				currentButton: "pause"
+			});
+
+			this.player.pause();
+		}
+	};
+
+	nextTrack = index => {
+		if (index >= this.state.songs.length) {
+			index = 0;
+		} else if (index < 0) {
+			index = 0;
+		}
+		this.setState({
+			j: index
+		});
+		this.playSong(index);
+	};
+
+	render() {
+		//this.player.onended(this.nextTrack(this.state.j + 1));
+		return (
+			<div>
+				<ol>
+					{this.state.songs.map((song, i) => {
+						return <li key={i}>{song.title}</li>;
+					})}
+				</ol>
+				<div>
+					<a onClick={() => this.nextTrack(this.state.j - 1)}>
+						<i className="fas fa-caret-square-left" />
+					</a>
+					<a onClick={() => this.playSong(this.state.j)}>
+						{this.state.currentAction}
+					</a>
+					<a onClick={() => this.nextTrack(this.state.j + 1)}>
+						<i className="fas fa-caret-square-right" />
+					</a>
+				</div>
+				<audio
+					src={this.state.currentSong.url}
+					ref={el => (this.player = el)}
+				/>
+			</div>
+		);
+	}
 }
