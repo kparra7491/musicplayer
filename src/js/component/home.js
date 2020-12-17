0945df39df43e9d75091913f0a3feb53fac13ab1
kparra7491 <kparra7491@gmail.com>
2020-12-17 19:50:13 +0000

    new buttons

--- src/js/component/home.js
+++ src/js/component/home.js
@@ -8,89 +8,176 @@ export class Home extends React.Component {
 		this.state = {
 			songs: [
 				{
-					title: "South Park",
+					title: "Blame Canada",
 					id: "south-park",
 					author: "Kyle",
 					url:
 						"https://assets.breatheco.de/apis/sound/files/cartoons/songs/south-park.mp3"
 				},
 				{
-					title: "Thunder Cats",
+					title: "Thunder Cats Theme",
 					id: "thundercats",
 					author: "Moonra",
 					url:
 						"https://assets.breatheco.de/apis/sound/files/cartoons/songs/thundercats.mp3"
 				},
 				{
-					title: "X-Men",
+					title: "X-Men Theme",
 					id: "x-men",
-					author: "Profesor",
+					author: "Profesor X",
 					url:
 						"https://assets.breatheco.de/apis/sound/files/cartoons/songs/x-men.mp3"
 				}
 			],
 			currentSong: {},
-			currentAction: <i className="fas fa-play" />,
-			currentButton: "",
 
-			j: 0
+			currentButton: "",
+			j: 0,
+			shuffle: "",
+			looping: false,
+			volume: 50
 		};
 	}
 
 	playSong = index => {
-		if (this.state.currentbutton !== "play") {
-			this.setState({
-				currentSong: this.state.songs[index],
-				currentAction: <i className="fas fa-play" />,
-				currentButton: "play"
-			});
-			this.player.play();
+		if (this.state.shuffle !== true) {
+			if (index >= this.state.songs.length) {
+				index = 0;
+			} else if (index < 0) {
+				index = 0;
+			}
 		} else {
-			this.setState({
-				currentAction: <i className="fas fa-pause-circle" />,
-				currentButton: "pause"
-			});
-
-			this.player.pause();
+			index = Math.floor(Math.random() * this.state.songs.length);
 		}
+
+		this.setState({ currentSong: this.state.songs[index], j: index });
+
+		this.player.play();
+		this.playButton.style.display = "none";
+		this.pauseButton.style.display = "inline";
 	};
 
-	nextTrack = index => {
-		if (index >= this.state.songs.length) {
-			index = 0;
-		} else if (index < 0) {
-			index = 0;
+	pauseSong = index => {
+		this.player.pause();
+		this.pauseButton.style.display = "none";
+		this.playButton.style.display = "inline";
+	};
+
+	random = () => {
+		this.setState({ shuffle: true });
+		this.shuffle.style.display = "none";
+		this.shuffleOff.style.display = "inline";
+	};
+	randomOff = () => {
+		this.setState({ shuffle: false });
+		this.shuffle.style.display = "inline";
+		this.shuffleOff.style.display = "none";
+	};
+
+	looper = bool => {
+		bool = !this.state.looping;
+		//this.player.current.loop = bool;
+		this.setState({ looping: bool });
+		this.state.looping
+			? (this.loop.style.opacity = 0.5)
+			: (this.loop.style.opacity = 1);
+
+		//opacity based on click is working. can't get the player.loop attribute to work
+	};
+
+	setVolume = vol => {
+		if (vol > 100) {
+			vol = 100;
+		} else if (vol < 0) {
+			vol = 0;
 		}
+
 		this.setState({
-			j: index
+			volume: vol
 		});
-		this.playSong(index);
+		//this.player.current.volume = vol;
 	};
+
+	volBar = vol => {
+		this.volBar.style.width = vol * 2 + "px";
+		//this.player.current.volume = vol;
+	};
+
+	componentDidMount() {}
 
 	render() {
-		//this.player.onended(this.nextTrack(this.state.j + 1));
 		return (
 			<div>
 				<ol>
 					{this.state.songs.map((song, i) => {
-						return <li key={i}>{song.title}</li>;
+						return (
+							<li onClick={() => this.playSong(i)} key={i}>
+								{song.title} - {song.author}
+							</li>
+						);
 					})}
 				</ol>
+
 				<div>
-					<a onClick={() => this.nextTrack(this.state.j - 1)}>
+					<a onClick={() => this.playSong(this.state.j - 1)}>
 						<i className="fas fa-caret-square-left" />
 					</a>
-					<a onClick={() => this.playSong(this.state.j)}>
-						{this.state.currentAction}
+					<a
+						ref={el => (this.playButton = el)}
+						onClick={() => this.playSong(this.state.j)}>
+						<i className="fas fa-play" />
 					</a>
-					<a onClick={() => this.nextTrack(this.state.j + 1)}>
+
+					<a
+						style={{ display: "none" }}
+						ref={el => (this.pauseButton = el)}
+						onClick={() => this.pauseSong()}>
+						<i className="fas fa-pause-circle" />
+					</a>
+					<a onClick={() => this.playSong(this.state.j + 1)}>
 						<i className="fas fa-caret-square-right" />
+					</a>
+					<a
+						ref={el => (this.shuffleOff = el)}
+						onClick={() => this.randomOff()}>
+						<i className="fas fa-random" />
+					</a>
+					<a
+						ref={el => (this.shuffle = el)}
+						onClick={() => this.random()}>
+						<i className="fas fa-sort-alpha-down" />
+					</a>
+					<a
+						ref={el => (this.loop = el)}
+						onClick={() => this.looper(this.state.looping)}>
+						<i className="fas fa-undo" />
+					</a>
+
+					<a
+						ref={el => (this.minus = el)}
+						onClick={() => this.setVolume(this.state.volume - 1)}>
+						<i className="fas fa-minus" />
+					</a>
+					<a
+						ref={el => (this.number = el)}
+						onChange={() => this.volBar(this.state.volume)}>
+						{this.state.volume}
+					</a>
+					<a
+						ref={el => (this.plus = el)}
+						onClick={() => this.setVolume(this.state.volume + 1)}>
+						<i className="fas fa-plus" />
 					</a>
 				</div>
 				<audio
+					volume={this.state.volume}
+					loop={this.state.looping}
 					src={this.state.currentSong.url}
 					ref={el => (this.player = el)}
 				/>
+
+				<div>{this.state.currentSong.title}</div>
+				<div>{this.state.currentSong.author}</div>
 			</div>
 		);
 	}
